7c4939de99a72f9a48e6e5fb05b818a46508b78e
tylernass <tyler.nass30@gmail.com>
2020-03-18 12:21:26 -0500

    Commit

--- simpleHttpServer.cpp
+++ simpleHttpServer.cpp
@@ -341,7 +341,7 @@ and return the response without PYTHON_PROMPT.
 
   // Model: numBytes	= read(fromPythonFd, LINE_BUFFER_LEN, stdin);
   // strip off PYTHON_PROMPT (of length PYTHON_PROMPT_LEN) from the end of the response,
- totalBuffer = read(fromPythonFd, buffer, LINE_BUFFER_LEN);
+ totalBuffer = read(fromPythonFd, buffer, LINE_BUFFER_LEN - strlen(PYTHON_PROMPT_LEN));
   // write(fd[1], totalBuffer, strlen(fromPythonFd) - PYTHON_PROMPT_LEN);
   //  YOUR CODE HERE
   return(totalBuffer);
